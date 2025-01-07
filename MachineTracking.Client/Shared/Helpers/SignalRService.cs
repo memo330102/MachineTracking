b44e77407ac01e61c3e92b33f9230b1e4b6f9efc
mehmetaksak <63792736+mehmetaksak@users.noreply.github.com>
2025-01-07 15:48:46 +0300

    signalr changes

--- MachineTracking.Client/Shared/Helpers/SignalRService.cs
+++ MachineTracking.Client/Shared/Helpers/SignalRService.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR.Client;
 using MachineTracking.Domain.Interfaces.Client;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MachineTracking.Client.Shared.Helpers
 {
@@ -7,12 +8,14 @@ namespace MachineTracking.Client.Shared.Helpers
     {
         private readonly HubConnection _hubConnection;
         private IConfiguration _configuration;
+        private readonly ILogger<SignalRService> _logger;
 
         public event Action<string> OnMachineDataReceived;
 
-        public SignalRService(IConfiguration configuration)
+        public SignalRService(IConfiguration configuration, ILogger<SignalRService> logger)
         {
-            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+            _configuration = configuration;
+            _logger = logger;
 
             var hubURL = $"{_configuration["ApiBaseUrl"]}{_configuration["SignalRSettings:HubName"]}";
             _hubConnection = new HubConnectionBuilder()
@@ -25,30 +28,53 @@ namespace MachineTracking.Client.Shared.Helpers
             if (!string.IsNullOrEmpty(hubMethodName))
             {
                 _hubConnection.On<string>(hubMethodName, data => OnMachineDataReceived?.Invoke(data));
+                _hubConnection.On<string>(hubMethodName, data =>
+                {
+                    _logger.LogInformation($"Data received from SignalR: {data}");
+                    OnMachineDataReceived?.Invoke(data);
+                });
             }
+            _logger.LogInformation($"SignalRService initialized with Hub URL: {hubURL}");
         }
 
         public async Task StartAsync()
         {
-            try
+
+            if (_hubConnection.State == HubConnectionState.Disconnected)
             {
-                if (_hubConnection.State == HubConnectionState.Disconnected)
+                try
                 {
                     await _hubConnection.StartAsync();
+                    _logger.LogInformation("SignalR connection started successfully.");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Failed to start SignalR connection.");
                 }
             }
-            catch (Exception ex)
+            else
             {
-
+                _logger.LogWarning($"SignalR connection is already in state: {_hubConnection.State}");
             }
-
         }
 
         public async Task StopAsync()
         {
             if (_hubConnection.State == HubConnectionState.Connected)
             {
-                await _hubConnection.StopAsync();
+                try
+                {
+                    await _hubConnection.StopAsync();
+                    _logger.LogInformation($"SignalR connection stopped successfully.");
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, $"Failed to stop SignalR connection.");
+                }
+            }
+            else
+            {
+                _logger.LogWarning($"SignalR connection is not in a connected state. Current state: {_hubConnection.State}");
             }
         }
     }
